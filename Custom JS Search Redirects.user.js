dfc02007fcf9da1fd0b03ec53388d01243e315d7
​ <>
2025-11-13 20:53:32 +0100

    Adding some more search engines, and not requiring a search string

--- Custom JS Search Redirects.user.js
+++ Custom JS Search Redirects.user.js
@@ -10,11 +10,25 @@
 
   const searchEngines = {
     a: 'https://www.amazon.com/s?k=%s',
-    'a fr': 'https://www.amazon.fr/s?k=%s',
+    'a au': 'https://www.amazon.com.au/s?k=%s',
+    'a be': 'https://www.amazon.com.be/s?k=%s',
+    'a br': 'https://www.amazon.com.br/s?k=%s',
+    'a ca': 'https://www.amazon.ca/s?k=%s',
     'a de': 'https://www.amazon.de/s?k=%s',
+    'a es': 'https://www.amazon.es/s?k=%s',
+    'a fr': 'https://www.amazon.fr/s?k=%s',
+    'a it': 'https://www.amazon.it/s?k=%s',
     'a jp': 'https://www.amazon.co.jp/s?k=%s',
     'a uk': 'https://www.amazon.co.uk/s?k=%s',
-    'a se': 'https://www.amazon.se/s?k=%s',
+    abnb: (searchTerm) => {
+      const transformed = searchTerm
+        .toLowerCase()
+        .replace(/,\s*/, '--')
+        .replace(/\s+/g, '-');
+      return `https://www.airbnb.com/s/${transformed}/homes`;
+    },
+    b: 'https://www.booking.com/searchresults.html?ss=%s',
+    m: 'https://www.google.com/maps/search/%s',
   };
 
   const url = new URL(window.location.href);
@@ -28,15 +42,15 @@
       return b.length - a.length;
     }
   )) {
-    const regex = new RegExp('^' + prefix + ' (.+)$');
+    const regex = new RegExp('^' + prefix + '(?: (.+))?$');
     const match = query.match(regex);
 
     if (match) {
-      const searchTerm = match[1];
-      const redirectUrl = urlTemplate.replace(
-        '%s',
-        encodeURIComponent(searchTerm)
-      );
+      const searchTerm = match[1] || '';
+      const redirectUrl =
+        typeof urlTemplate === 'function'
+          ? urlTemplate(searchTerm)
+          : urlTemplate.replace('%s', encodeURIComponent(searchTerm));
       window.location.replace(redirectUrl);
       return;
     }
