0f1214360e1db50ac61109da8a984e74b9cd83b9
​ <>
2025-11-13 20:34:14 +0100

    Sorting search prefixes by specificity

--- Custom JS Search Redirects.user.js
+++ Custom JS Search Redirects.user.js
@@ -5,29 +5,38 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
   const searchEngines = {
+    a: 'https://www.amazon.com/s?k=%s',
     'a fr': 'https://www.amazon.fr/s?k=%s',
     'a de': 'https://www.amazon.de/s?k=%s',
     'a jp': 'https://www.amazon.co.jp/s?k=%s',
     'a uk': 'https://www.amazon.co.uk/s?k=%s',
     'a se': 'https://www.amazon.se/s?k=%s',
-    'a': 'https://www.amazon.com/s?k=%s',
   };
 
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
   const query = params.get('q');
 
-  for (const [prefix, urlTemplate] of Object.entries(searchEngines)) {
+  for (const [prefix, urlTemplate] of Object.entries(searchEngines).sort(
+    ([a], [b]) => {
+      if (b.startsWith(a)) return 1;
+      if (a.startsWith(b)) return -1;
+      return b.length - a.length;
+    }
+  )) {
     const regex = new RegExp('^' + prefix + ' (.+)$');
     const match = query.match(regex);
-    
+
     if (match) {
       const searchTerm = match[1];
-      const redirectUrl = urlTemplate.replace('%s', encodeURIComponent(searchTerm));
+      const redirectUrl = urlTemplate.replace(
+        '%s',
+        encodeURIComponent(searchTerm)
+      );
       window.location.replace(redirectUrl);
       return;
     }
