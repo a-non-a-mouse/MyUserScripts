705b39c314671d81185f416d939cc27bf538ae3d
​ <>
2025-11-15 11:46:09 +0100

    Adding date parsing to airbnb/booking shortcuts

--- Custom JS Search Redirects.user.js
+++ Custom JS Search Redirects.user.js
@@ -5,6 +5,53 @@
 // @run-at       document-start
 // ==/UserScript==
 
+function parseDatesFromSearchTerm(searchTerm) {
+  let match;
+  const dateRangeRegex = /(\d{1,2}\/\d{1,2})-(\d{1,2}\/\d{1,2})/;
+
+  if ((match = searchTerm.match(dateRangeRegex))) {
+    const startDate = match[1];
+    const endDate = match[2];
+    const cleanedSearchTerm = searchTerm.replace(dateRangeRegex, '').trim();
+    const today = new Date();
+    const currentYear = today.getFullYear();
+
+    const parseDate = (dateStr) => {
+      const [month, day] = dateStr.split('/').map(Number);
+      const date = new Date(currentYear, month - 1, day, 12, 0, 0, 0);
+
+      if (date < today) {
+        date.setFullYear(currentYear + 1);
+      }
+
+      return date;
+    };
+
+    const startDateObj = parseDate(startDate);
+    const endDateObj = parseDate(endDate);
+
+    return { searchTerm: cleanedSearchTerm, dates: [startDateObj, endDateObj] };
+  }
+
+  return { searchTerm, dates: null };
+}
+
+function getDateQueryParams(
+  dates,
+  fromDateParam = 'checkin',
+  toDateParam = 'checkout',
+  dateFormatter = (date) => date.toISOString().split('T')[0]
+) {
+  if (!dates) {
+    return '';
+  }
+  const [checkin, checkout] = dates;
+
+  return `${fromDateParam}=${dateFormatter(
+    checkin
+  )}&${toDateParam}=${dateFormatter(checkout)}`;
+}
+
 (function () {
   'use strict';
 
@@ -20,14 +67,23 @@
     'a it': 'https://www.amazon.it/s?k=%s',
     'a jp': 'https://www.amazon.co.jp/s?k=%s',
     'a uk': 'https://www.amazon.co.uk/s?k=%s',
-    abnb: (searchTerm) => {
-      const transformed = searchTerm
-        .toLowerCase()
-        .replace(/,\s*/, '--')
-        .replace(/\s+/g, '-');
-      return `https://www.airbnb.com/s/${transformed}/homes`;
+    air: (inputSearchTerm) => {
+      const { searchTerm, dates } = parseDatesFromSearchTerm(inputSearchTerm);
+      const transformed = searchTerm.replace(/,\s*/, '--').replace(/\s+/g, '-');
+      const url = `https://www.airbnb.com/s/${transformed}/homes?adults=1&${getDateQueryParams(
+        dates
+      )}`;
+
+      return url;
     },
-    b: 'https://www.booking.com/searchresults.html?ss=%s',
+    b: (inputSearchTerm) => {
+      const { searchTerm, dates } = parseDatesFromSearchTerm(inputSearchTerm);
+      const url = `https://www.booking.com/searchresults.html?ss=${searchTerm}&group_adults=1&no_rooms=1&group_children=0&${getDateQueryParams(
+        dates
+      )}`;
+
+      return url;
+    },
     m: 'https://www.google.com/maps/search/%s',
   };
 
@@ -42,7 +98,7 @@
       return b.length - a.length;
     }
   )) {
-    const regex = new RegExp('^' + prefix + '(?: (.+))?$');
+    const regex = new RegExp(`^${prefix}(?: (.+))?$`);
     const match = query.match(regex);
 
     if (match) {
